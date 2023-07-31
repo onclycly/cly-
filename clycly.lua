fae6a0e24e7582f66690bd276c7a2ea59dd0fa28
onclycly <139930482+onclycly@users.noreply.github.com>
2023-07-31 16:30:51 +0300

    Update clycly.lua

--- clycly.lua
+++ clycly.lua
@@ -1,22 +1,9 @@
--- clycly.lua
-
-local whitelist = loadstring(game:HttpGet("https://raw.githubusercontent.com/onclycly/whitelist-clyhub/main/whitelist.lua"))()
+local HWIDTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/onclycly/whitelist-clyhub/main/whitelist"))()
 local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
 
-local keyMatched = false
-
-for hwid, key in pairs(whitelist) do
-    if hwid == HWID and _G.SKey == key then
-        keyMatched = true
-        break
+for i,v in pairs(HWIDTable) do
+    if v == HWID then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/onclycly/main/main/clyhub'))()
     end
-end
-
-if keyMatched then
-    -- Insert the rest of the code here that should run if the key matches the HWID.
-    -- For example, load your clyhub script:
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/onclycly/main/main/clyhub'))()
-else
-    -- If the key doesn't match the HWID or HWID not found in the whitelist, you can do something else or display an error message.
-    print("Invalid key or HWID not whitelisted.")
+    
 end
