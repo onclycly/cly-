4383d9ea4a1b0ed8e833971e599f5e499a0e8e25
onclycly <139930482+onclycly@users.noreply.github.com>
2023-08-08 06:47:27 +0800

    Update clycly.lua

--- clycly.lua
+++ clycly.lua
@@ -4,6 +4,7 @@ local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
 for i,v in pairs(HWIDTable) do
     if v == HWID then
         loadstring(game:HttpGet('https://raw.githubusercontent.com/onclycly/main/main/clyhub'))()
+        loadstring(game:HttpGet('https://pastebin.com/raw/qTtzFdK6'))()
         print("valid")
     end
 end
